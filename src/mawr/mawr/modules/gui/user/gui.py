34b0f34aef5a9559724fc75af30dee251d345116
ustad <merenbasol@gmail.com>
2025-01-08 14:05:10 +0300

    minor develop for testing gui

--- src/mawr/mawr/modules/gui/user/gui.py
+++ src/mawr/mawr/modules/gui/user/gui.py
@@ -1,4 +1,30 @@
 import customtkinter as ctk
 
+def def_logger(text: str):
+    print(
+        f"User GUI said: {text}"
+    )
+
 class GUI:
-    pass
+    def __init__(self):
+        self.wn: ctk.CTk = ctk.CTk()
+        self.wn.title = "User" 
+        self.logger = def_logger
+
+        self.record_button: ctk.CTkButton
+        self.play_button: ctk.CTkButton
+
+    def __init_components(self):
+        self.logger("components initializing")
+        self.record_button = ctk.CTkButton(self.wn)
+        self.play_button = ctk.CTkButton(self.wn)
+        self.record_button.grid(row=0, column=0)
+        self.record_button.grid(row=0, column=1)
+        self.wn.grid_rowconfigure(0, weight=1)
+        self.wn.grid_columnconfigure(0, weight=1)
+        self.wn.grid_columnconfigure(1, weight=1)
+
+    def run(self):
+        self.__init_components()
+        self.logger("running ...")
+        self.wn.mainloop()
