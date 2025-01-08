b09dfd11003b6b63861244a95aac8af7d6174e28
ustad <merenbasol@gmail.com>
2025-01-08 10:34:52 +0300

    minor changes

--- src/mawr/mawr/modules/message/directional.py
+++ src/mawr/mawr/modules/message/directional.py
@@ -1,7 +1,7 @@
 from .message import Message
 
 class DirectionalMessage(Message):
-    def __init__(self):
+    def __init__(self, from_: str = None, to_: str = None):
         super().__init__()
         self.from_: str = None
         self.to_: str = None
