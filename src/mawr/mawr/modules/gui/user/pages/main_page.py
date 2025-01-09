5833f152300f814515f27ef7a685fe2291c35e05
ustad <merenbasol@gmail.com>
2025-01-09 13:09:27 +0300

    some changes in ui

--- src/mawr/mawr/modules/gui/user/pages/main_page.py
+++ src/mawr/mawr/modules/gui/user/pages/main_page.py
@@ -7,26 +7,38 @@ class MainPage(ctk.CTkFrame):
         super().__init__(master)
         self.record_button: ctk.CTkButton
         self.play_button: ctk.CTkButton
-        self.togg = True
+
+        self.play_state: False
+        self.record_state: False
+        
         self.e_play: Event[Int] = Event("play", Int)
         self.e_record: Event[Bool] = Event("record", Bool)
 
     def __cb_play_button(self):
         print("Play button pressed")
-        self.e_play.trigger(0) 
+        if self.play_state:
+            self.play_button.configure(text="play")
+            self.e_play.trigger(-1) 
+        else:
+            self.play_button.configure(text="pause")
+            self.e_play.trigger(0) 
     
+        self.play_state = not self.play_state
+
     def __cb_record_button_pressed(self):
         print("Record button pressed")
-        self.e_record.trigger(self.togg)
-        self.togg = not self.togg 
-
-    def __cb_record_button_released(self):
-        print("Record button released")
-        self.e_record.trigger(False)
+        if self.record_state:
+            self.record_button.configure(text="record")
+            self.e_record.trigger(False)
+        else:
+            self.record_button.configure(text="save")
+            self.e_record.trigger(True)
+        
+        self.record_state = not self.record_state
 
     def create(self):
-        self.record_button = ctk.CTkButton(self, text="Record", command=self.__cb_record_button_pressed)
-        self.play_button = ctk.CTkButton(self, text="Play", command=self.__cb_play_button)
+        self.record_button = ctk.CTkButton(self, text="record", command=self.__cb_record_button_pressed)
+        self.play_button = ctk.CTkButton(self, text="play", command=self.__cb_play_button)
 
         # self.record_button.bind("<ButtonRelease-1>", lambda e: self.__cb_record_button_released())
 
