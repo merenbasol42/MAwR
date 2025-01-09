e7fc870dc02ea770f808081d6f50f8224e7b4f9d
ustad <merenbasol@gmail.com>
2025-01-09 12:07:59 +0300

    add frame functionlaty added to Message class

--- src/mawr/mawr/modules/message/message.py
+++ src/mawr/mawr/modules/message/message.py
@@ -3,5 +3,8 @@ class Message:
     def __init__(self):
         self.audio: list[list[int]] = []
 
-    def add_part(self, part: list[int]):
+    def add_part(self, part: list[list[int]]):
         self.audio.extend(part)
+
+    def add_frame(self, frame: list[int]):
+        self.audio.append(frame)
